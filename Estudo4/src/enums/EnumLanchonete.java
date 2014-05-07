f0505c6c0a0e771ba769229afd142d551ed1713a
Marcio <mar_geo_jp@yahoo.com.br>
2014-05-07 10:11:25 -0300

    Criação do menu de opções.

--- Estudo4/src/enums/EnumLanchonete.java
+++ Estudo4/src/enums/EnumLanchonete.java
@@ -42,4 +42,26 @@ public class EnumLanchonete {
 		}
 		
 	}
+
+	
+	public static void menu(){
+		System.out.println("Escolha a sua opção no menu: ");
+		System.out.println("0. SAIR");
+		System.out.println("==================================");
+		System.out.println("\tBEBIDAS");
+		System.out.println("1. " + Bebida.CocaCola.getNome() + "R$ "+Bebida.CocaCola.getPreco());
+		System.out.println("2. " + Bebida.Agua.getNome() + "R$ " + Bebida.Agua.getPreco());
+		System.out.println("3. " + Bebida.Suco.getNome() + "R$ "+Bebida.Suco.getPreco());
+		System.out.println("==================================");
+		System.out.println("\n\n");
+		System.out.println("==================================");
+		System.out.println("\tCOMIDAS");
+		System.out.println("4. " + Comida.HotDog.getNome() + "R$ " + Comida.HotDog.getPreco());
+		System.out.println("5. " + Comida.Sanduiche.getNome() + "R$ " + Comida.Sanduiche.getPreco() );
+		System.out.println("6. " + Comida.Xburger.getNome() + "R$ " + Comida.Xburger.getPreco());		
+		System.out.println("==================================");
+	}
+	
+	
+
 }
